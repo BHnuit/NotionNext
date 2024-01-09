95df5d25dab24a2f2a20b5922af37e2862a13e6a
BHnuit <34975191+BHnuit@users.noreply.github.com>
2024-01-09 12:12:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -29,8 +29,8 @@ const BLOG = {
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || 'https://movie.douban.com/people/144104707', // 你的linkedIn 首页，已替换为豆瓣电影
-  CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || 'https://www.xiaohongshu.com/user/profile/614c73e2000000000201eb92', // 您的instagram地址，已替换为豆瓣读书
-  CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || 'https://book.douban.com/people/144104707', // B站主页，已替换为小红书
+  CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || 'https://book.douban.com/people/144104707', // 您的instagram地址，已替换为豆瓣读书
+  CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || 'https://www.xiaohongshu.com/user/profile/614c73e2000000000201eb92', // B站主页，已替换为小红书
   CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || 'https://open.spotify.com/user/31z7xqx65rve65fw5kr4yraqlafq', // Youtube主页，已替换为Spotify
 
   NOTION_HOST: process.env.NEXT_PUBLIC_NOTION_HOST || 'https://www.notion.so', // Notion域名，您可以选择用自己的域名进行反向代理，如果不懂得什么是反向代理，请勿修改此项
