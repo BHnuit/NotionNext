75e938cf22123560fa04eeb8be3496f12bd60262
BHnuit <34975191+BHnuit@users.noreply.github.com>
2024-06-04 11:52:51 +0800

    修改部分社交图标

--- themes/fukasawa/components/SocialButton.js
+++ themes/fukasawa/components/SocialButton.js
@@ -17,14 +17,14 @@ const SocialButton = () => {
       {siteConfig('CONTACT_TELEGRAM') && <a target='_blank' rel='noreferrer' href={siteConfig('CONTACT_TELEGRAM')} title={'telegram'} >
         <i className='fab fa-telegram transform hover:scale-125 duration-150'/>
       </a>}
-      {siteConfig('CONTACT_LINKEDIN') && <a target='_blank' rel='noreferrer' href={siteConfig('CONTACT_LINKEDIN')} title={'linkedIn'} >
-        <i className='fab fa-linkedin transform hover:scale-125 duration-150'/>
+      {siteConfig('CONTACT_LINKEDIN') && <a target='_blank' rel='noreferrer' href={siteConfig('CONTACT_LINKEDIN')} title={'最近看的电影'} >
+        <i className='fas fa-film transform hover:scale-125 duration-150'/>
       </a>}
       {siteConfig('CONTACT_WEIBO') && <a target='_blank' rel='noreferrer' title={'weibo'} href={siteConfig('CONTACT_WEIBO')} >
         <i className='fab fa-weibo transform hover:scale-125 duration-150'/>
       </a>}
-      {siteConfig('CONTACT_INSTAGRAM') && <a target='_blank' rel='noreferrer' title={'instagram'} href={siteConfig('CONTACT_INSTAGRAM')} >
-        <i className='fab fa-instagram transform hover:scale-125 duration-150'/>
+      {siteConfig('CONTACT_INSTAGRAM') && <a target='_blank' rel='noreferrer' title={'最近读的书'} href={siteConfig('CONTACT_INSTAGRAM')} >
+        <i className='fas fa-book transform hover:scale-125 duration-150'/>
       </a>}
       {siteConfig('CONTACT_EMAIL') && <a target='_blank' rel='noreferrer' title={'email'} href={`mailto:${siteConfig('CONTACT_EMAIL')}`} >
         <i className='fas fa-envelope transform hover:scale-125 duration-150'/>
@@ -35,8 +35,8 @@ const SocialButton = () => {
       {siteConfig('CONTACT_BILIBILI') && <a target='_blank' rel='noreferrer' title={'bilibili'} href={siteConfig('CONTACT_BILIBILI')} >
         <i className='fab fa-bilibili transform hover:scale-125 duration-150'/>
       </a>}
-      {siteConfig('CONTACT_YOUTUBE') && <a target='_blank' rel='noreferrer' title={'youtube'} href={siteConfig('CONTACT_YOUTUBE')} >
-        <i className='fab fa-youtube transform hover:scale-125 duration-150'/>
+      {siteConfig('CONTACT_YOUTUBE') && <a target='_blank' rel='noreferrer' title={'最近听的歌'} href={siteConfig('CONTACT_YOUTUBE')} >
+        <i className='fab fa-spotify transform hover:scale-125 duration-150'/>
       </a>}
     </div>
   </div>
